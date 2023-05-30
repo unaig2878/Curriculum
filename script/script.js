cdfbaeea47c8608a7ff66647b9952bb0fc8ac4c9
unaig2878 <unaig2878@gmail.com>
2023-05-30 10:58:24 +0200

    Añadido los cards

---
+++ script/script.js
@@ -0,0 +1,26 @@
+// Obtener todas las tarjetas pequeñas
+var smallCards = document.querySelectorAll('.small-card');
+
+// Obtener la tarjeta grande
+var largeCard = document.querySelector('.large-card');
+
+// Recorrer todas las tarjetas pequeñas y agregar un evento de clic a cada una
+smallCards.forEach(function(card) {
+  card.addEventListener('click', function() {
+
+    // Cogemos la imagen y el contenido de lo que se haga click
+    var cardContent = card.querySelector('.content-2').innerHTML;
+    var cardImage = card.querySelector('img').getAttribute('src');
+
+    // Actualizar el contenido y la imagen de la tarjeta grande
+    largeCard.querySelector('.content-2').innerHTML = cardContent;
+    largeCard.querySelector('img').setAttribute('src', cardImage);
+    
+  });
+});
+
+
+
+
+
+
